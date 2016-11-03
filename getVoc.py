72bd204cd71ec7143d8ba405d2bf09e44c40481f
Alex <sasha-ushakov@mail.ru>
2016-11-03 22:16:13 +0300

    4

--- getVoc.py
+++ getVoc.py
@@ -4,23 +4,25 @@ import xml.etree.ElementTree as ET
 import vsptd
 
 
-def trp_get_voc_db(p, n, t):
-
-    # для БД на mssql
+# для БД на mssql
+def trp_get_voc_mssql(p, n, t):
     engine = sqlalchemy.create_engine('mssql+pyodbc://PC-ALEX/P3375_K_SBD_5_1_Ushakov?driver=ODBC+Driver+11+for+SQL+Server')
     query = sqlalchemy.text('SELECT LINK FROM OSl_test_1 WHERE OBJ=:prefix and NAME=:name')
     result = engine.execute(query,{"prefix": p, "name": n}).fetchall()[0]
     for row in result:
-        print(vsptd.parse_triplex_string(row))
+        return vsptd.parse_triplex_string(row)
 
-    # для БД на sqlite
+
+# для БД на sqlite
+def trp_get_voc_sqlite(p, n, t):
     engine = sqlalchemy.create_engine(r'sqlite:///' + t)
     sql_query = 'SELECT LINK FROM OSl_test_1 WHERE OBJ=:prefix and NAME=:name'
     result = engine.execute(sql_query, prefix=p, name=n).fetchall()[0]
     for row in result:
-        print(vsptd.parse_triplex_string(row))
+        return vsptd.parse_triplex_string(row)
 
 
+# для БД на xml
 def trp_get_voc_xml(p, n, t):
     tree = ET.ElementTree(file=t)
     root = tree.getroot()
@@ -29,11 +31,12 @@ def trp_get_voc_xml(p, n, t):
         nam = r.find("NAME").text
         if pre == p and nam == n:
             link = r.find("LINK").text
-            print(vsptd.parse_triplex_string(link))
+            return vsptd.parse_triplex_string(link)
 
 prefix = "E"
 name = "D"
 db_path = r'E:\Document\ИТМО\5 семестр\СБД\P3375_К_СБД_5_1_Ушаков\SBDcourse5sem\test_sql_db_1.db'
 xml_path = r'E:\Document\ИТМО\5 семестр\СБД\P3375_К_СБД_5_1_Ушаков\SBDcourse5sem\test_sql_db_2.xml'
-trp_get_voc_db(prefix, name, db_path)
-trp_get_voc_xml(prefix, name, xml_path)
+print(trp_get_voc_mssql(prefix, name, db_path))
+print(trp_get_voc_sqlite(prefix, name, db_path))
+print(trp_get_voc_xml(prefix, name, xml_path))
