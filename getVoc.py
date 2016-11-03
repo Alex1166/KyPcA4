d0da1bf6b23f6f1419b2e7d05f88888f6347b885
Alex <sasha-ushakov@mail.ru>
2016-11-03 22:08:19 +0300

    3

--- getVoc.py
+++ getVoc.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sqlalchemy
 import xml.etree.ElementTree as ET
+import vsptd
 
 
 def trp_get_voc_db(p, n, t):
@@ -8,18 +9,16 @@ def trp_get_voc_db(p, n, t):
     # для БД на mssql
     engine = sqlalchemy.create_engine('mssql+pyodbc://PC-ALEX/P3375_K_SBD_5_1_Ushakov?driver=ODBC+Driver+11+for+SQL+Server')
     query = sqlalchemy.text('SELECT LINK FROM OSl_test_1 WHERE OBJ=:prefix and NAME=:name')
-    # print(query)
     result = engine.execute(query,{"prefix": p, "name": n}).fetchall()[0]
     for row in result:
-        print(row)
+        print(vsptd.parse_triplex_string(row))
 
     # для БД на sqlite
     engine = sqlalchemy.create_engine(r'sqlite:///' + t)
     sql_query = 'SELECT LINK FROM OSl_test_1 WHERE OBJ=:prefix and NAME=:name'
-
     result = engine.execute(sql_query, prefix=p, name=n).fetchall()[0]
     for row in result:
-        print(row)
+        print(vsptd.parse_triplex_string(row))
 
 
 def trp_get_voc_xml(p, n, t):
@@ -30,7 +29,7 @@ def trp_get_voc_xml(p, n, t):
         nam = r.find("NAME").text
         if pre == p and nam == n:
             link = r.find("LINK").text
-            print(link)
+            print(vsptd.parse_triplex_string(link))
 
 prefix = "E"
 name = "D"
