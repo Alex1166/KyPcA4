4b3fd726e818bd0569f3b674d9d25139aabfd358
Alex <sasha-ushakov@mail.ru>
2016-11-16 02:20:29 +0300

    В функцию trpGetOntVoc добавлена возможность вывода нескольких записей в зависимости от передаваемых префикса и имени Дополнены комментарии

--- ontVoc.py
+++ ontVoc.py
@@ -11,14 +11,14 @@ import xml.etree.ElementTree as ET
 def trpGetOntVoc(prefix, name, path_db, type_db, table_name=''):
     """
     Получение словаря метаданных из базы данных
-            Принимает:
-                prefix - префикс реквизита
-                name - имя реквизита
-                path_db - путь к файлу, содержащему словари метаданных
-                type_db - язык, на котором написана БД: 0 - xml, 1 - sqlite, 2 - mssql
-                table_name - имя таблицы, содержащей словарь, в этой БД
-            Возвращает:
-                (TrpStr)
+        Принимает:
+            prefix (str) - префикс реквизита
+            name (str) - имя реквизита
+            path_db (str) - путь к файлу, содержащему словари метаданных
+            type_db (str) or (int) - язык, на котором написана БД: 0 - xml, 1 - sqlite, 2 - mssql
+            table_name (str) - имя таблицы, содержащей словарь, в этой БД
+        Возвращает:
+            [(TrpStr)] - список с данными в виде триплексных строк
     """
 
     if type_db == 'XML' or type_db == 0:
@@ -30,23 +30,48 @@ def trpGetOntVoc(prefix, name, path_db, type_db, table_name=''):
 
         # получение структуры XML-базы
         root = tree.getroot()
+        str_list = []
+        str_list_value, trp_str_list_prefix, trp_str_list_name = [], [], []
         triplex_string = TrpStr()
         # перебор всех записей в БД
         for data in root.findall('ROWDATA'):
+            # занесение всех строк в виде триплексных строк в список
             for row in data.iter('ROW'):
                 attributes = row.attrib
-                # выбор записей по совпадению префикса и имени
-                if attributes.get('Q.OBJ') == prefix and attributes.get('Q.NAME') == name:
-                    for attr in attributes:
-                        xml_value = attributes.get(attr)
-                        if attr != 'RowState':
-                            if re.match(RE_TRIPLET, xml_value) is None:
-                                # занесение данных из записи в триплексную строку
-                                xml_prefix, xml_name = attr.split('.')
-                                triplex_string += Trp(xml_prefix, xml_name, xml_value)
-                            else:
-                                triplex_string += parse_trp_str(xml_value)
-        return triplex_string
+                for attr in attributes:
+                    xml_value = attributes.get(attr)
+                    if attr != 'RowState':
+                        if re.match(RE_TRIPLET, xml_value) is None:
+                            # занесение данных из записи в триплексную строку
+                            xml_prefix, xml_name = attr.split('.')
+                            triplex_string += Trp(xml_prefix, xml_name, xml_value)
+                        else:
+                            triplex_string += parse_trp_str(xml_value)
+                str_list.append(triplex_string)
+
+        # выбор записей по совпадению префикса и имени
+        for trp_str in str_list:
+            if trp_str['Q.OBJ'] == prefix and trp_str['Q.NAME'] == name:
+                str_list_value.append(trp_str)
+            elif trp_str['Q.OBJ'] == prefix:
+                trp_str_list_prefix.append(trp_str)
+            elif trp_str['Q.NAME'] == name:
+                trp_str_list_name.append(trp_str)
+
+        if prefix == '':
+            # если не было введено ни имя реквизита, ни префикс, функция вернет все строки из таблицы
+            if name == '':
+                return str_list
+            # если было введено только имя реквизита, функция вернет все строки с совпадающим именем
+            else:
+                return trp_str_list_name
+        else:
+            # если имя реквизита не было введено, функция вернет все строки с совпадающим префиксом
+            if name == '':
+                return trp_str_list_prefix
+            # если имя реквизита было введено, функция вернет одну строку с совпадающими префиксом и именем
+            else:
+                return str_list_value
 
     else:
         # подключение к базе данных
@@ -66,32 +91,47 @@ def trpGetOntVoc(prefix, name, path_db, type_db, table_name=''):
             col_list.append(col)
             pref_list.append(col.name.split('.')[0])
             name_list.append(col.name.split('.')[1])
+
         # SQL-запрос для получения данных из таблицы по совпадению префикса и имени
-        sql_query = q_table.select().where(and_(col_list[0] == prefix, col_list[1] == name))
-        result = engine.execute(sql_query).fetchone()
-        # занесение полученных данных в триплексную строку
-        triplex_string = TrpStr(Trp(pref_list[0], name_list[0], result[0]),
-                                Trp(pref_list[1], name_list[1], result[1]),
-                                Trp(pref_list[2], name_list[2], result[2]),
-                                Trp(pref_list[3], name_list[3], result[3]),
-                                Trp(pref_list[4], name_list[4], result[4]),
-                                )
-        triplex_string += parse_trp_str(result[5])
-        return triplex_string
+        if prefix == '':
+            # если не было введено ни имя реквизита, ни префикс, функция вернет все строки из таблицы
+            if name == '':
+                sql_query = q_table.select()
+            # если было введено только имя реквизита, функция вернет все строки с совпадающим именем
+            else:
+                sql_query = q_table.select().where(col_list[1] == name)
+        else:
+            # если имя реквизита не было введено выбираются все строки с совпадающим префиксом
+            if name == '':
+                sql_query = q_table.select().where(col_list[0] == prefix)
+            # если имя реквизита было введено, выбирается одну строку с совпадающими префиксом и именем
+            else:
+                sql_query = q_table.select().where(and_(col_list[0] == prefix, col_list[1] == name))
+        result = engine.execute(sql_query).fetchall()
+        res_list = []
+        for res in result:
+            # занесение полученных данных в триплексную строку
+            triplex_string = TrpStr(Trp(pref_list[0], name_list[0], res[0]),
+                                    Trp(pref_list[1], name_list[1], res[1]),
+                                    Trp(pref_list[2], name_list[2], res[2]),
+                                    Trp(pref_list[3], name_list[3], res[3]),
+                                    Trp(pref_list[4], name_list[4], res[4]),
+                                    )
+            triplex_string += parse_trp_str(res[5])
+            res_list.append(triplex_string)
+        return res_list
 
 
 def trpPutOntVoc(prefix, name, trp_voc, path_db, type_db, table_name=''):
     """
     Запись в словарь сторки метаданных, представленной в триплетной форме
-            Принимает:
-                prefix - префикс реквизита
-                name - имя реквизита
-                trp_voc – триплетная строка метаданных (TrpStr)
-                path_db - путь к файлу, содержащему словари метаданных
-                type_db - язык, на котором написана БД: 0 - xml, 1 - sqlite, 3 - mssql
-                table_name - имя таблицы, содержащей словарь, в этой БД
-            Возвращает:
-                (TrpStr)
+        Принимает:
+            prefix (str) - префикс реквизита
+            name (str) - имя реквизита
+            trp_voc (TrpStr) – триплетная строка метаданных
+            path_db (str) - путь к файлу, содержащему словари метаданных
+            type_db (str) or (int) - язык, на котором написана БД: 0 - xml, 1 - sqlite, 3 - mssql
+            table_name (str) - имя таблицы, содержащей словарь, в этой БД
     """
 
     if type_db == 'XML' or type_db == 0:
@@ -172,8 +212,8 @@ def sql_engine(type_db, path_db):
     """
     Функция создания движка базы данных
         Принимает:
-            type_db - язык, на котором написана БД: 0 - xml, 1 - sqlite, 3 - mssql
-            path_db - путь к файлу, содержащему словари метаданных
+            type_db (str) or (int)  - язык, на котором написана БД: 0 - xml, 1 - sqlite, 3 - mssql
+            path_db (str) - путь к файлу, содержащему словари метаданных
         Возвращает:
             (Engine)
     """
@@ -201,7 +241,7 @@ def indentXML(elem, level=0):
     Функция для создания отступов
             Принимает:
                 elem - элемент XML файла
-                level - уровень начального отступа
+                level (int) - уровень начального отступа
     """
 
     i = "\n" + level*"    "
--- tests/getVoc_test.py
+++ tests/getVoc_test.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 import sys
-import getVoc
-import putVoc
-import vsptd
 import ontVoc
 
 sys.path.append('..\\')
 
+# prefix = ""
+# name = ""
 
 prefix = "E"
 name = "D"
@@ -25,16 +24,62 @@ trpVoc = "$Q.NM='Диаметр';$VERT.NAME='DIAMETR';$SPRUT.NAME='Diam';$Q.FRMT
 # trpVoc = "$VERT.NAME='LEN';$SPRUT.NAME='Dlin';$Q.NAME='L';$Q.NM='Длина';$Q.FRMT='999V99';$Q.K='1';$Q.OBJ='E';"
 # trpVoc = "$Q.NM='Длина';$Q.K='1';$VERT.NAME='LEN';$SPRUT.NAME='Dlin';$Q.FRMT='999V99';"
 
-# ------------------XML----------------------------
-print(ontVoc.trpGetOntVoc(prefix, name, type_db='XML', path_db='tests/db/test_sql_db_3.xml'))
-# print(ontVoc.trpPutOntVoc(prefix, name, trpVoc, type_db='XML', path_db='tests/db/test_sql_db_3.xml'))
-
-
-# ------------------SQLite-------------------------
+# # ------------------XML----------------------------
+print(ontVoc.trpGetOntVoc(prefix, name, path_db='tests/db/test_sql_db_3.xml', type_db='XML'))
+# print(ontVoc.trpPutOntVoc(prefix, name, trpVoc, path_db='tests/db/test_sql_db_3.xml', type_db='XML'))
+#
+#
+# # ------------------SQLite-------------------------
 print(ontVoc.trpGetOntVoc(prefix, name, path_db='tests/db/test_sql_db_1.sqlite', type_db='SQLite', table_name='OSl_test_1'))
-# print(ontVoc.trpPutOntVoc(prefix, name, trpVoc, type_db='SQLite', path_db='tests/db/test_sql_db_1.sqlite', table_name='OSl_test_1'))
+# print(ontVoc.trpPutOntVoc(prefix, name, trpVoc, path_db='tests/db/test_sql_db_1.sqlite', type_db='SQLite', table_name='OSl_test_1'))
+#
+#
+# # ------------------MS SQL Server------------------
+# print(ontVoc.trpGetOntVoc(prefix, name, path_db='PC-ALEX/P3375_K_SBD_5_1_Ushakov', type_db='MS SQL Server', table_name='OSl_test_1'))
+# print(ontVoc.trpPutOntVoc(prefix, name, trpVoc, path_db='PC-ALEX/P3375_K_SBD_5_1_Ushakov', type_db='MS SQL Server', table_name='OSl_test_1'))
 
 
-# ------------------MS SQL Server------------------
-# print(ontVoc.trpGetOntVoc(prefix, name, type_db='MS SQL Server', path_db='PC-ALEX/P3375_K_SBD_5_1_Ushakov', table_name='OSl_test_1'))
-# print(ontVoc.trpPutOntVoc(prefix, name, trpVoc, type_db='MS SQL Server', path_db='PC-ALEX/P3375_K_SBD_5_1_Ushakov', table_name='OSl_test_1'))
+# # ------------------XML----------------------------
+# ontVoc.trpGetOntVoc(prefix,
+#                     name,
+#                     path_db='tests/db/test_sql_db_3.xml',
+#                     type_db='XML'
+#                     )
+# ontVoc.trpPutOntVoc(prefix,
+#                     name,
+#                     trpVoc,
+#                     path_db='tests/db/test_sql_db_3.xml',
+#                     type_db='XML'
+#                     )
+#
+#
+# # --------------SQLite-------------------------
+# ontVoc.trpGetOntVoc(prefix,
+#                     name,
+#                     path_db='tests/db/test_sql_db_1.sqlite',
+#                     type_db='SQLite',
+#                     table_name='OSl_test_1'
+#                     )
+# ontVoc.trpPutOntVoc(prefix,
+#                     name,
+#                     trpVoc,
+#                     path_db='tests/db/test_sql_db_1.sqlite',
+#                     type_db='SQLite',
+#                     table_name='OSl_test_1'
+#                     )
+#
+#
+# # --------------MS SQL Server------------------
+# ontVoc.trpGetOntVoc(prefix,
+#                     name,
+#                     path_db='PC-ALEX/P3375_K_SBD_5_1_Ushakov',
+#                     type_db='MS SQL Server',
+#                     table_name='OSl_test_1'
+#                     )
+# ontVoc.trpPutOntVoc(prefix,
+#                     name,
+#                     trpVoc,
+#                     path_db='PC-ALEX/P3375_K_SBD_5_1_Ushakov',
+#                     type_db='MS SQL Server',
+#                     table_name='OSl_test_1'
+#                     )
